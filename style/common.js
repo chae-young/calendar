9370f9be2b9ad3b321141611e7b52509b6f8f3e9
chae-young <cycy8527@gmail.com>
2021-08-26 19:33:54 +0900

    Fix height style

--- style/common.js
+++ style/common.js
@@ -1,4 +1,7 @@
 import styled from "styled-components"
+import { weekLength } from "../components/CalendarTodo"
+
+export const pointColor = "rgb(119 221 118)"
 
 export const val = {
   headerHeight: 50,
@@ -7,13 +10,18 @@ export const val = {
     return this.headerHeight + this.rowHeight
   },
 }
-
 export const CalendarRow = styled.div`
+  position: relative;
   display: flex;
-  height: ${(props) => (props.day ? "calc(100%  / 5)" : `${val.rowHeight}px`)};
+  height: ${(props) =>
+    props.day ? `calc(100%  / ${weekLength})` : `${val.rowHeight}px`};
+  border-top: 1px solid rgb(0, 0, 0);
   > div {
     flex: 1;
+
     height: ${(props) => (props.day ? "" : `${val.rowHeight}px`)};
+    line-height: ${(props) => (props.day ? "" : `${val.rowHeight}px`)};
     text-align: ${(props) => (props.day ? "left" : "center")};
+    border-left: 1px solid rgb(0, 0, 0);
   }
 `
