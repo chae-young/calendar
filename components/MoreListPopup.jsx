a6c14b65cd1928b4b8497a0b49e813abde28ad90
chae-young <cycy8527@gmail.com>
2021-08-26 19:32:52 +0900

    Fix Morelist style

--- components/MoreListPopup.jsx
+++ components/MoreListPopup.jsx
@@ -1,42 +1,53 @@
 import React, { useState, useEffect, useCallback } from "react"
+
 import { X } from "react-bootstrap-icons"
-import style from "styled-components"
+
+import styled from "styled-components"
 import EditPopup from "./EditPopup"
 
-const MoreListPopWrap = style.div`
-    position:absolute;
-    left:50%;
-    top:0;
-    transform:translateX(-50%);
-    width:calc(100% + 10px);
-    background:#fff;
-    -webkit-box-shadow: 0px 1px 11px 5px rgba(0,0,0,0.33); 
-    box-shadow: 0px 1px 11px 5px rgba(0,0,0,0.33); 
-    
-    & button{
-        position:absolute;
-        right:0;
-        top:0;
-    }
+const MoreListPopWrap = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translateX(-50%);
+  width: calc(100% + 10px);
+  background: #fff;
+  -webkit-box-shadow: 0px 1px 11px 5px rgba(0, 0, 0, 0.33);
+  box-shadow: 0px 1px 11px 5px rgba(0, 0, 0, 0.33);
+
+  & button {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
 `
 
-const MoreListPopup = ({ post }) => {
-  console.log(post)
+const MoreListPopup = ({ post, setMoreClick }) => {
+  console.log("모얼리스트")
   const [editPopup, setEditPopup] = useState(false)
   const [currentList, setCurrentList] = useState([])
   const onClickPop = (list) => {
     setCurrentList((prev) => [list])
-    setModifyPop(true)
+    setEditPopup(true)
   }
+  const onClose = useCallback(() => {
+    setMoreClick(false)
+  }, [])
   return (
     <MoreListPopWrap onClick={(e) => e.stopPropagation()}>
       {/* <strong>{list[0].weekDay}</strong> */}
       <ul>
         {post.map((v, i) => (
-          <li onClick={(post) => onClickPop(v)}>{v.content.title}</li>
+          <li
+            key={v.currentDate}
+            style={{ backgroundColor: `${v.bgColor}` }}
+            onClick={(post) => onClickPop(v)}
+          >
+            {v.content.title}
+          </li>
         ))}
       </ul>
-      <button>
+      <button onClick={onClose}>
         <X />
       </button>
       {editPopup && <EditPopup currentList={currentList} />}
